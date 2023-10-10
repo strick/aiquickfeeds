3f275486487981256d3a294bd8a800b167f0f3fc
Brian Strickland <bstrick@gmail.com>
2023-10-10 14:15:09 -0400

    Filter results by one

--- helpers/articleHelper.js
+++ helpers/articleHelper.js
@@ -10,7 +10,6 @@ export async function getArticleLinks(htmlContent) {
   
         const link = headlineFigure.attr('href');   // Extract the article's link.
         const title = $(headlineFigure).find('img').attr('alt').trim();
-        //console.log(title + ": " + link);
         const dateElement = $(element).find('time');
         const pubDate = dateElement.attr('datetime') || dateElement.text().trim(); // Get the publication date.
   
@@ -20,7 +19,7 @@ export async function getArticleLinks(htmlContent) {
     });
   
     return articles;
-  }
+}
   
 export async function getTechRadarArticleLinks(htmlContent) {
     const $ = cheerio.load(htmlContent);
@@ -41,7 +40,45 @@ export async function getTechRadarArticleLinks(htmlContent) {
   
     return articles;
 }
+/*
+// Usage for the first set of selectors:
+const config1 = {
+    articleSelector: 'article.sc-cw4lnv-0',
+    anchorSelector: 'figure > a.sc-1out364-0',
+    titleSelector: 'img',
+    dateSelector: 'time'
+};
 
+const articles1 = await extractArticleLinks(htmlContent, config1);
+
+// Usage for the second set of selectors:
+const config2 = {
+    articleSelector: 'div.listingResult',
+    anchorSelector: 'a.article-link',
+    titleSelector: 'h3.article-name',
+    dateSelector: 'time'
+};
+
+export async function extractArticleLinks(htmlContent, config) {
+    const $ = cheerio.load(htmlContent);
+    const articles = [];
+
+    $(config.articleSelector).each((index, element) => {
+
+        const anchor = $(element).find(config.anchorSelector);
+        const link = anchor.attr('href');
+        const title = $(anchor).find(config.titleSelector).text().trim();
+        const dateElement = $(anchor).find(config.dateSelector);
+        const pubDate = dateElement.attr('datetime') || dateElement.text().trim();
+
+        if (title && link) {
+            articles.push({ title, link, pubDate });
+        }
+    });
+
+    return articles;
+}
+*/
 export async function getOpenAIReleaseNotes(htmlContent){
   const $ = cheerio.load(htmlContent);
     const articles = [];
