7ec6b6d5db9441720cc47fe99af39e328e7056ce
Brian Strickland <bstrick@gmail.com>
2023-10-13 23:42:17 -0400

    Remove MS for now

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ export const feedUrls = [
     { url: 'https://www.ai.gov/feed/', title: 'NAIIO'},
     { url: 'https://news.mit.edu/topic/mitartificial-intelligence2-rss.xml', title: 'MIT News'},
     { url: 'https://blogs.nvidia.com/blog/category/deep-learning/feed/', title: 'NVIDIA'},
-    { url: 'https://techcommunity.microsoft.com/plugins/custom/microsoft/o365/custom-blog-rss?tid=-3314434996627025474&board=AICustomerEngineeringTeam&size=10', title:'MS: AI Customer Engineering'},
+   // { url: 'https://techcommunity.microsoft.com/plugins/custom/microsoft/o365/custom-blog-rss?tid=-3314434996627025474&board=AICustomerEngineeringTeam&size=10', title:'MS: AI Customer Engineering'},
    // { url: 'https://techcommunity.microsoft.com/plugins/custom/microsoft/o365/custom-blog-rss?tid=-3314434996627025474&board=MachineLearningBlog&size=10', title: 'MS: AI Machine Learning'},
    // { url: 'https://techcommunity.microsoft.com/plugins/custom/microsoft/o365/custom-blog-rss?tid=-3314434996627025474&board=Azure-AI-Services-blog&size=10', title: 'MS: Azure AI Services'}
   ];
